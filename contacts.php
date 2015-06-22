91745b73f3a0f3e246484fe870aea1d24b368d2f
Alexey <SmiteVils@yandex.ru>
2015-06-22 13:13:23 +0300

    Верстаю Контакты

---
+++ contacts.php
@@ -0,0 +1,6 @@
+<?php include_once "assets/html/head.html" ?>
+<body>
+	<?php include_once "assets/plugins/mobile-menu/mobile-menu.html" ?>
+	<?php include_once "assets/html/main-menu-white.html" ?>
+	<?php include_once "assets/html/contacts.html" ?>
+</body>
