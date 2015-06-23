47a920eb7829a160c8628826776205f7e446b970
Alexey <SmiteVils@yandex.ru>
2015-06-23 11:52:21 +0300

    Верстаю страницу новости

---
+++ news.php
@@ -0,0 +1,7 @@
+<?php include_once "assets/html/head.html" ?>
+<body>
+	<?php include_once "assets/plugins/mobile-menu/mobile-menu.html" ?>
+	<?php include_once "assets/html/main-menu-white.html" ?>
+	<?php include_once "assets/html/news.html" ?>
+	<?php include_once "assets/html/footer.html" ?>
+</body>
