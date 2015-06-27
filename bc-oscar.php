f87091413e03522825021fd8373075af62bf635c
Alexey <SmiteVils@yandex.ru>
2015-06-27 15:52:22 +0300

    Добавил скрипт скрытия текста

---
+++ bc-oscar.php
@@ -0,0 +1,7 @@
+<?php include_once "assets/html/head.html" ?>
+<body>
+	<?php include_once "assets/plugins/mobile-menu/mobile-menu.html" ?>
+	<?php include_once "assets/html/main-menu.html" ?>
+	<?php include_once "assets/html/bc-oscar.html" ?>
+	<?php include_once "assets/html/footer.html" ?>
+</body>
