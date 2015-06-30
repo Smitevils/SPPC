0ec99f8c9ee345794f9e489b6bdf82e7641f2d78
Alexey <SmiteVils@yandex.ru>
2015-06-30 10:33:54 +0300

    Добавил POPUP

--- aboutus.php
+++ aboutus.php
@@ -1,5 +1,6 @@
 <?php include_once "assets/html/head.html" ?>
 <body>
+	<?php include_once "assets/html/popup.html" ?>
 	<?php include_once "assets/plugins/mobile-menu/mobile-menu.html" ?>
 	<?php include_once "assets/html/main-menu-white.html" ?>
 	<?php include_once "assets/html/aboutus.html" ?>
